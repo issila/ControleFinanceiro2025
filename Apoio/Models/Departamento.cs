5b55ee4441b471d0ca661b9176ce93af48bcfe7c
Breno Carvalho <103794884+BrenoC4rvalho@users.noreply.github.com>
2025-05-20 20:23:43 -0300

    feat: modal instituicao

---
+++ Apoio/Models/Departamento.cs
@@ -0,0 +1,9 @@
+﻿namespace Apoio.Models
+{
+    public class Departamento
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public decimal Orcamento{ get; set; }
+    }
+}
